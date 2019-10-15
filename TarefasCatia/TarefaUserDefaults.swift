98d4f7ccfae62c31ccefbcb563a8cd7a3222b77b
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-10-15 14:30:12 -0300

    Finalizando o app com o delete

--- TarefasCatia/TarefaUserDefaults.swift
+++ TarefasCatia/TarefaUserDefaults.swift
@@ -7,6 +7,15 @@ class TarefaUserDefaults {
     let chave = "listaTarefa"
     var tarefas: [String] = []
     
+    //REMOVER ATRAVES DO DELETE
+    func remover(indice: Int) {
+        
+        tarefas = listar()
+        tarefas.remove(at: indice)
+        UserDefaults.standard.set(tarefas, forKey: chave)
+        
+    }
+    
     func salvar(tarefa: String){
         
         //RECUPERAR TAREFA JA SALVA
--- TarefasCatia/TarefasTableViewController.swift
+++ TarefasCatia/TarefasTableViewController.swift
@@ -4,31 +4,55 @@ import UIKit
 
 class TarefasTableViewController: UITableViewController {
 
+    var tarefas: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+      
 
        
+    }
+    // MOSTRAR O DELETE E DELETAR
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCell.EditingStyle.delete{
+            
+            let tarefa = TarefaUserDefaults()
+            tarefa.remover(indice: indexPath.row)
+            atualizarListaTarefas()
+        }
+      
+    }
+    func atualizarListaTarefas(){
+        let tarefa = TarefaUserDefaults()
+        tarefas = tarefa.listar()
+        tableView.reloadData()
+    }
+    //METODO Q E CARREGADO TODA VEZ Q A TELA CARREGA P USUARIO
+    override func viewDidAppear(_ animated: Bool) {
+         atualizarListaTarefas()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return tarefas.count
     }
 
    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let celula = tableView.dequeueReusableCell(withIdentifier: "celula", for: indexPath)
 
-        // Configure the cell...
+        celula.textLabel?.text = tarefas[indexPath.row]
 
-        return cell
+        return celula
     }
     
 
